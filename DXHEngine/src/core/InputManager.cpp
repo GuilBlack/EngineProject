64eaca77bef319b5bd7efdec52f0f7e066f3037f
Jun <jzhi@gaming.tech>
2023-10-25 18:09:25 +0200

    Update InputManager.cpp

--- DXHEngine/src/core/InputManager.cpp
+++ DXHEngine/src/core/InputManager.cpp
@@ -2,5 +2,10 @@
 
 void InputManager::CheckInputs()
 {
+	if (m_KeyState.empty())
+	{
+		m_KeyState.resize(m_Keys.size(), KeyState::NONE);
+	}
 
+	vector<KeyState> currentKeyState(m_Keys.size(), KeyState::NONE);
 }
