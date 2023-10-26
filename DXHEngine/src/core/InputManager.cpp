4f107758c5ed9804fcc4210cf44cdb7c0369a216
Jun <jzhi@gaming.tech>
2023-10-26 14:18:54 +0200

    Update InputManager.cpp

--- DXHEngine/src/core/InputManager.cpp
+++ DXHEngine/src/core/InputManager.cpp
@@ -8,4 +8,16 @@ void InputManager::CheckInputs()
 	}
 
 	vector<KeyState> currentKeyState(m_Keys.size(), KeyState::NONE);
+
+	for (size_t i = 0; i < m_Keys.size(); ++i)
+	{
+		if (GetAsyncKeyState(m_Keys[i]) < 0) {
+			currentKeyState[i] = (m_KeyState[i] == KeyState::HELD || m_KeyState[i] == KeyState::DOWN) ? KeyState::HELD : KeyState::DOWN;
+		}
+		else
+		{
+			currentKeyState[i] = (m_KeyState[i] == KeyState::HELD || m_KeyState[i] == KeyState::DOWN) ? KeyState::UP : KeyState::NONE;
+		}
+	}
+	m_KeyState = currentKeyState;
 }
