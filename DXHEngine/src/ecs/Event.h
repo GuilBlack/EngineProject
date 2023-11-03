e00af07c83b1351caa01bba0800029a540a84098
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2023-11-03 22:54:42 +0100

    Fix the Event class
    I corrected the names of the member variables and methods + initialized m_Functions member variable.

--- DXHEngine/src/ecs/Event.h
+++ DXHEngine/src/ecs/Event.h
@@ -3,38 +3,38 @@
 #include <vector>
 template<typename ...T> class Event
 {
-	Event() {};
+	Event() : m_Functions() {};
 
 	inline void operator += (const std::function<void(Args...)>& function) 
 	{
-		m_functions.push_back(function);
+		m_Functions.push_back(function);
 	}
 	inline void operator += (const Event& event)
 	{
-		m_functions.insert(m_functions.end(), event.m_functions.begin(), event.m_functions.end());
+		m_Functions.insert(m_Functions.end(), event.m_Functions.begin(), event.m_Functions.end());
 	}
 	inline void operator -= (const std::function<void(Args...)>& function)
 	{
-		m_functions.erase(std::remove(m_functions.begin(), m_functions.end(), function), m_functions.end());
+		m_Functions.erase(std::remove(m_Functions.begin(), m_Functions.end(), function), m_Functions.end());
 	}
 
 	void operator()(Args... args)
 	{
-		for (auto& function : m_functions)
+		for (auto& function : m_Functions)
 		{
 			function(args...);
 		}
 	}
 
-	inline void invoke(Args... args)
+	inline void Invoke(Args... args)
 	{
 		operator()(args...);
 	}
-	inline void clear()
+	inline void Clear()
 	{
-		m_functions.clear();
+		m_Functions.clear();
 	}
 
 private :
-	std::vector<std::function<void(Args...)>> m_functions;
+	std::vector<std::function<void(Args...)>> m_Functions;
 };
