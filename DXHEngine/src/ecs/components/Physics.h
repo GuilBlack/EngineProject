d9f04ca2ce3a6f3dd3454f685e6d578c7916e9e8
Lucas Vocanson <lvocanson@gaming.tech>
2023-11-08 10:46:28 +0100

    Add Collision data struct

--- DXHEngine/src/ecs/components/Physics.h
+++ DXHEngine/src/ecs/components/Physics.h
@@ -27,11 +27,17 @@ struct SphereCollider : Component
     {
         Center = Vector3::Zero;
         Radius = 0.f;
+        CollisionLayer = 1;
+        CollisionMask = 1;
     }
     Vector3 Center = Vector3::Zero; // Relative to game object
-    float Radius = 0.f;
+    float Radius = 0.f; // In meters
+    unsigned char CollisionLayer = 1; // The layer this collider is on
+    unsigned char CollisionMask = 1; // The layers this collider collides with
 
     // Calculate the position of the collider in world space
     inline Vector3 WorldPosition() { return pGameObject->Position() + Center; }
+    // Returns true if the collider collides with the other collider
+    inline bool CanCollidesWith(SphereCollider& other) { return (CollisionLayer & other.CollisionMask) != 0; }
 };
 }
